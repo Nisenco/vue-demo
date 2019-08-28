c334b8d5b66df37d9d1df079c3bff283cea102dd
nico <wangmeng1@social-touch.com>
2019-08-28 16:19:11 +0800

    install vuex

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Vuex from 'vuex'
 import HelloWorld from '@/components/HelloWorld'
-
-Vue.use(Router)
+Vue.use(Vuex);
+Vue.use(Router);
 
 export default new Router({
   routes: [
